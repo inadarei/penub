8bd65dd70d63a4152e366e1183767cf1154c15e5
inadarei <irakli@gmail.com>
2010-05-04 09:03:39 -0400

    adding config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define ('PENUB_DB', 'mysql://user:password@server/db_name');
+define ('PENUB_KERNEL', 'penub');
+define ('PENUB_CONTRIB', 'penub-contrib');
+define ('PENUB_CUSTOM_CODE', 'custom');
+define ('PENUB_FILES_FOLDER', 'files');
+
