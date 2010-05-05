14ec423bdbbf6779c9281d0808414d07cf5dedb2
inadarei <irakli@gmail.com>
2010-05-05 17:57:16 -0400

    refactoring and implementing router

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-if (file_exists(dirname(__FILE__) . '/config.php') {
+if (file_exists(dirname(__FILE__) . '/config.php')) {
   require_once ('config.php');
 }
 else {
   define('PENUB_KERNEL_PATH', 'penub');
 }
 
-require_once (PENUB_KERNEL_PATH . '/core/penub.kernel.php');
+require_once (PENUB_KERNEL_PATH . '/core/penub.kernel.inc');
