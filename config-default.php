c7643aa562699ac48bd98e6bf8cb9c5be854aa1a
inadarei <irakli@gmail.com>
2010-05-05 23:35:58 -0400

    implemented modules auto-discovery

--- config-default.php
+++ config-default.php
@@ -3,9 +3,12 @@
 define ('PENUB_DB', 'mysql://user:password@server/db_name');
 
 define ('PENUB_KERNEL_PATH', 'framework');
-define ('PENUB_CONTRIB_PATH', 'penub-contrib');
+define ('PENUB_CONTRIB_PATH', 'pcontrib');
 define ('PENUB_CUSTOM_CODE_PATH', 'custom');
+
 define ('PENUB_FILES_FOLDER_PATH', 'files');
+define ('PENUB_LIB_FOLDER_PATH', PENUB_KERNEL_PATH . '/libraries');
+define ('PENUB_RUNTIMECONF_FOLDER_PATH', PENUB_FILES_FOLDER_PATH . '/runtimeconf');
 
 define ('PENUB_LOG_LEVEL_NOTICE',  1);
 define ('PENUB_LOG_LEVEL_DEBUG',   2);
--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
 if (file_exists(dirname(__FILE__) . '/config.php')) {
-  require_once ('config.php');
+  require ('config.php');
 }
 else {
   die("Configuration file not found! Please make sure there's config.php in the root folder.");
 }
 
-require_once (PENUB_KERNEL_PATH . '/core/penub.kernel.inc');
+require (PENUB_KERNEL_PATH . '/core/penub.kernel.inc');
 
 
 $kernel = new PenubKernel();
