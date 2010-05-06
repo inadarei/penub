5fedf66551a96ad2ddbe2748e479bb3486af4b2c
inadarei <irakli@gmail.com>
2010-05-05 22:15:06 -0400

    renaming penub/penub to framework to reduce confussion

--- config-default.php
+++ config-default.php
@@ -2,7 +2,7 @@
 
 define ('PENUB_DB', 'mysql://user:password@server/db_name');
 
-define ('PENUB_KERNEL_PATH', 'penub');
+define ('PENUB_KERNEL_PATH', 'framework');
 define ('PENUB_CONTRIB_PATH', 'penub-contrib');
 define ('PENUB_CUSTOM_CODE_PATH', 'custom');
 define ('PENUB_FILES_FOLDER_PATH', 'files');
