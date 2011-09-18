598ce85c30c26bdd4d43fb6b2e660ead9eba9e1c
Irakli Nadareishvili <irakli@gmail.com>
2011-09-18 00:28:48 -0400

    clean

--- www/index.php
+++ www/index.php
@@ -1,14 +1,21 @@
 <?php
 
-if (file_exists(dirname(__FILE__) . '/config.php')) {
-  require ('config.php');
+phpinfo();
+die();
+
+
+$config_file = realpath(dirname(__FILE__) . '/../config.php');
+if (file_exists($config_file)) {
+  require ($config_file);
 }
 else {
   die("Configuration file not found! Please make sure there's config.php in the root folder.");
 }
 
+require ( PENUB_KERNEL_PATH . '/core/penub.kernel.inc');
+
+
 penub_log("Request processing launched");
-require (PENUB_KERNEL_PATH . '/core/penub.kernel.inc');
 
 $kernel = new PenubKernel();
 $kernel->run();
