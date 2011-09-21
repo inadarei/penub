d2df47ccc84491209d541bc06534666a410e1c94
Irakli Nadareishvili <irakli@gmail.com>
2011-09-20 22:46:42 -0400

    playing with stylesheet

--- js/site.js
+++ js/site.js
@@ -1,5 +1,30 @@
 $('document').ready(function(){
   
+  function getElementsByTagNames(list,obj) {
+  	if (!obj) var obj = document;
+  	var tagNames = list.split(',');
+  	var resultArray = new Array();
+  	for (var i=0;i<tagNames.length;i++) {
+  		var tags = obj.getElementsByTagName(tagNames[i]);
+  		for (var j=0;j<tags.length;j++) {
+  			resultArray.push(tags[j]);
+  		}
+  	}
+  	var testNode = resultArray[0];
+  	if (!testNode) return [];
+  	if (testNode.sourceIndex) {
+  		resultArray.sort(function (a,b) {
+  				return a.sourceIndex - b.sourceIndex;
+  		});
+  	}
+  	else if (testNode.compareDocumentPosition) {
+  		resultArray.sort(function (a,b) {
+  				return 3 - (a.compareDocumentPosition(b) & 6);
+  		});
+  	}
+  	return resultArray;
+  }
+  
   function createTOC() {
   	var y = document.createElement('div');
   	y.id = 'innertoc';
