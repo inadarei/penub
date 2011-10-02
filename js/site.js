4294e5872d1e73b6580d8fdcf9a449c883795b5f
Irakli Nadareishvili <irakli@gmail.com>
2011-10-02 12:14:17 -0400

    portamento

--- js/site.js
+++ js/site.js
@@ -73,5 +73,5 @@ $('document').ready(function(){
   document.getElementById('column').appendChild(createTOC());
   showhideTOC();
 
-  //$('#column').portamento();	
+  $('#column').portamento();	
 });
